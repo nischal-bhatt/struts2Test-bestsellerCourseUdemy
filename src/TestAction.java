01c709661c04c0201a59e98f11e3ab98d9b20184
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-04 17:29:11 +0800

    committing very first simple struts project

---
+++ src/TestAction.java
@@ -0,0 +1,31 @@
+
+public class TestAction {
+
+	String message;
+	String firstName;
+	
+	public String execute()
+	{
+		System.out.println("execute() method called");
+		message = "SUCCESS Message";
+		System.out.println("Name: "+firstName);
+		return "success";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+}
