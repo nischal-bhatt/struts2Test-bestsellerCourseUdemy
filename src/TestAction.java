5d3bf3a3982307690be1095d4bef70268461d279
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-05 16:05:02 +0800

    committing

--- src/TestAction.java
+++ src/TestAction.java
@@ -1,15 +1,18 @@
+import com.opensymphony.xwork2.Action;
 
-public class TestAction {
+//ActionSupport class implements Action interface
+public class TestAction implements Action{
 
 	String message;
 	String firstName;
 	
+	//implements
 	public String execute()
 	{
 		System.out.println("execute() method called");
 		message = "SUCCESS Message";
 		System.out.println("Name: "+firstName);
-		return "success";
+		return SUCCESS;
 	}
 
 	public String getMessage() {
